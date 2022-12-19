bfb5bbed5abc419a22a655ce0de2880ff42804a7
Dhairya Chheda <dhairyakchheda@gmail.com>
2022-12-19 22:34:04 +0530

    Final commit

--- script.js
+++ script.js
@@ -9,12 +9,16 @@ toggle.addEventListener("click", () => {
   sidebar.classList.toggle("close");
 });
 
+searchBtn.addEventListener("click", () => {
+  sidebar.classList.remove("close");
+});
+
 modeSwitch.addEventListener("click", () => {
   body.classList.toggle("dark");
 
   if (body.classList.contains("dark")) {
-    modeText.textContent = "Light Mode";
+    modeText.innerText = "Light Mode";
   } else {
-    modeText.textContent = "Dark Mode";
+    modeText.innerText = "Dark Mode";
   }
 });
